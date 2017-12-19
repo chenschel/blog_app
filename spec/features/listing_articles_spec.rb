7e08099aa1db2b8393afb40d40f5578ab45f5f7c
Christopher Henschel <christopher.henschel@sumcumo.com>
2017-12-19 14:45:26 +0100

    listing articles

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -5,4 +5,16 @@ RSpec.feature 'Listing Articles' do
     @article1 = Article.create(title: 'Title of first article', body: 'Body of the first aritcle')
     @article2 = Article.create(title: 'Title of seconde article', body: 'Body of the second aritcle')
   end
+
+  scenario 'List all Articles' do
+    visit '/'
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+  end
 end
