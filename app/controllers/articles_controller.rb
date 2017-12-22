df0d1e97c16b82b04d19cc942f24b690177ea96a
Christopher Henschel <christopher.henschel@sumcumo.com>
2017-12-22 20:47:12 +0100

    styleing devise message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
   before_action :article, only: %i[edit update show destroy]
   def index
     @articles = Article.all
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map do |msg|
+      content_tag(:li, msg)
+    end.join
+    html = <<-HTML
+<div class="error error-alert error-danger">
+  <a href="#" class="close" data-dismiss='alert' #215;></a>
+<%= content_tag(:div, msg if msg.is_a?(String)) %>
+</div>
+    HTML
+    html.html_safe
+  end
 end
